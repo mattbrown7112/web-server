9887a200f46bbe103a86efd4cb2ab69ef5f4dade
Matt Brown <mbrown@ctechnologiesinc.com>
2016-07-15 15:36:25 -0400

    server updated

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000; //heroku
 
 // middleware can be attached to specific routes or the whole application
 var middleware = require('./middleware.js');
